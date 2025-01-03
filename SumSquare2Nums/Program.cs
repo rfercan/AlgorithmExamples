3c08b0e62e52a3487982ceadda2b3df9e3679971
Fatih <r.fatihercan@hotmail.com>
2025-01-03 22:52:14 +0300

    Add project files.

---
+++ SumSquare2Nums/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumSquare2Nums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum,num1,num2;
+            Console.Write("Enter num1: " );
+            num1=Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Enter num2: ");
+            num2=Convert.ToInt32( Console.ReadLine() ) ;
+
+            sum = (num1*num1) + (num2*num2);
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
