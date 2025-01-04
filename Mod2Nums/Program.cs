816d3c5efefa96437063383ea472ac9254b27b45
Fatih <r.fatihercan@hotmail.com>
2025-01-04 09:16:51 +0300

    Finding the mode of the entered numbers

---
+++ Mod2Nums/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Mod2Nums
+{/*Finding the mode of the entered numbers*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("enter num1: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter num2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            while (num1 >= num2)
+            {
+                // num1 = num1 - num2;
+                num1 = num1 % num2;
+            }
+
+            //for (int i = 0; num1 >= num2; i++) 
+            //{
+            //       // num1 = num1 - num2;
+            //        num1 = num1 % num2;
+            //}
+
+
+            Console.WriteLine("Result: "+num1);
+        }
+    }
+}
