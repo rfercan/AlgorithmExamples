99177147384d1212510a471d5294c911b9cb8181
Fatih <r.fatihercan@hotmail.com>
2025-01-04 08:59:00 +0300

    Finding division using subtraction

---
+++ ExtractInsteadofDivide/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExtractInsteadofDivide
+{/*Finding division using subtraction*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num1,num2;
+
+            Console.Write("Enter num1: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter num2: ");
+            num2 =Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 1; num1 > num2; i++)
+            {
+                num1 = num1 - num2;
+            }
+            Console.WriteLine("Result :" + i);
+        }
+    }
+}
