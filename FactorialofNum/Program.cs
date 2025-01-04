bb240fad466dabf7648b0ba769f5af83864fdde6
Fatih <r.fatihercan@hotmail.com>
2025-01-04 08:17:11 +0300

    Finding the factorial of the entered number

---
+++ FactorialofNum/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Diagnostics.Contracts;
+
+namespace FactorialofNum
+{/*Finding the factorial of the entered number*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fac = 1, num;
+            Console.Write("Enter a number :");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = num; i > 1; i--) {
+                fac = fac * i;
+               
+            }
+            Console.WriteLine("Factorial:" + fac);
+        }
+    }
+}
