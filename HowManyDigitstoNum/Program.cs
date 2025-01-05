b65cd9537670dac6e6111eae323b4ec6c3659901
Fatih <r.fatihercan@hotmail.com>
2025-01-05 22:07:52 +0300

    Finding how many digits the entered number

---
+++ HowManyDigitstoNum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HowManyDigitstoNum
+{/*Finding how many digits the entered number*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, result= 1;
+            Console.Write("Enter a num: ");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = result; num >= 9; result++)
+            //{
+            //    num = num / 10;
+
+            //}
+
+            while (num >= 9) {
+
+                num = num / 10;
+                result++;
+            }
+
+            
+            
+            
+            Console.WriteLine(result);
+          
+        }
+    }
+}
