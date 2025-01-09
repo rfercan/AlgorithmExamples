2925d258894b6cd908c9e26c98315e16405b68c4
Fatih <r.fatihercan@hotmail.com>
2025-01-09 23:08:35 +0300

    finding the sum of negative numbers,  the product of even numbers and the piece of numbers equal to 7

---
+++ NumEqualto7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NumEqualto7
+{
+    internal class Program
+    {/* finding the sum of negative numbers,
+ the product of even numbers and the piece of numbers equal to 7*/
+        static void Main(string[] args)
+        {
+            int num, negative = 0, even = 1, equal = 0;
+
+            for (int i = 0; i < 25; i++)
+            {
+
+                Console.Write("enter num : ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2 == 0)
+                    even *= num;
+
+                else if (num < 0)
+                    negative += num;
+
+                else if (num == 7)
+                    equal++;
+            }
+            Console.WriteLine("Even : "+even+ " "+"Negative : "+negative+ " "+"Equal: "+equal);
+        }
+    }
+}
