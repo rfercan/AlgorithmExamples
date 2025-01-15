aaa9b6463640ad956e0826b977062b3d65826bdb
Fatih <r.fatihercan@hotmail.com>
2025-01-15 22:27:09 +0300

    printing the multiplication table

---
+++ MultiplicationTtable/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MultiplicationTtable
+{
+    internal class Program
+    {/*printing the multiplication table*/
+        static void Main(string[] args)
+        {
+            int i, j, result;
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine();
+                for (j = 1; j <= 10; j++)
+                {
+                    result = i * j;
+                    Console.WriteLine(i + " * " + j + " = " + result);
+                }
+            }
+                
+               
+            Console.ReadLine();
+        }
+    }
+}
